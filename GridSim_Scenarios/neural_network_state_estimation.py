107b24fc0965cfa1b96a0cb7ca632e1269408e9d
Cosmin Ginerica <cosmin.ginerica@elektrobit.com>
2019-05-23 16:56:04 +0300

    Further work on World Model

---
+++ GridSim_Scenarios/neural_network_state_estimation.py
@@ -0,0 +1,97 @@
+from world_model import AgentAction, AgentObservation, WorldModel
+import random
+
+
+possible_actions = [AgentAction.DOWN, AgentAction.RIGHT, AgentAction.UP, AgentAction.LEFT, AgentAction.STAY]
+
+num_moving_obstacles = 5
+
+obstacle_encoders = [
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1]
+]
+
+
+def generate_action_sequences(num_sequences, history_size, prediction_horizon):
+    """
+    Generate random sequences of actions of fixed length (size of agent history + size of prediction horizon)
+    :param num_sequences: number of sequences to generate
+    :param history_size: size of agent's history
+    :param prediction_horizon: size of prediction horizon
+    :return: list of sequences
+    """
+    seq_list = list()
+    for _ in range(num_sequences):
+        crt_len = history_size + prediction_horizon
+        sequence = list()
+        for _ in range(crt_len):
+            sequence.append(random.choice(possible_actions, k=1))
+        seq_list.append(sequence)
+    return seq_list
+
+
+def build_observations(action_sequence, gridsim_object):
+    """
+    Simulate using GridSim the vehicle's actions and construct observations based on these actions and the simulation
+    environment
+    :param action_sequence: sequence of the vehicle's actions
+    :param gridsim_object: simulator handle
+    :return: observations corresponding to each action from the input sequence
+    """
+    observations = list()
+    for action in action_sequence:
+        car_pos, walls, cars = gridsim_object.advance_and_get_observation(action)
+        observations.append(AgentObservation(car_pos, walls, cars))
+    return observations
+
+
+def prepare_training_data(sequence_list, history_size, prediction_horizon, gridsim_object):
+    """
+    Generate observations based on the action (sequence list)
+    :param sequence_list: list of action sequences of length history_size + prediction_horizon
+    :param history_size: size of history
+    :param prediction_horizon: size of future prediction
+    :param gridsim_object: simulator handle
+    :return: (history_observations, prediction_observations)
+    """
+    history_observations = list()
+    prediction_observations = list()
+    for act_seq in sequence_list:
+        observations = build_observations(act_seq, gridsim_object)
+        h_obs = observations[:history_size]
+        pred_obs = observations[-prediction_horizon:]
+        history_observations.append(h_obs)
+        prediction_observations.append(pred_obs)
+    return history_observations, prediction_observations
+
+
+class WorldModelAlgorithm(object):
+    def __init__(self, gridsim_object, history_size, prediction_size, training_set_size):
+        self.gridsim_object = gridsim_object
+        self.history_size = history_size
+        self.prediction_size = prediction_size
+        self.training_set_size = training_set_size
+        self.gridsim_object.start_simulation()
+        self.history_observations, self.prediction_observations = None, None
+
+    def get_training_data(self):
+        action_sequences = generate_action_sequences(num_sequences=self.training_set_size,
+                                                     history_size=self.history_size,
+                                                     prediction_horizon=self.prediction_size)
+        self.history_observations, self.prediction_observations = \
+            prepare_training_data(sequence_list=action_sequences,
+                                  history_size=self.history_size,
+                                  prediction_horizon=self.prediction_size,
+                                  gridsim_object=self.gridsim_object)
+
+    def train_world_model_network(self):
+        pass
+
+    def save_model(self):
+        pass
+
+    def load_model(self):
+        pass
--- GridSim_Scenarios/world_model.py
+++ GridSim_Scenarios/world_model.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 class WorldModel(object):
-    def __init__(self, prediction_horizon_size):
+    def __init__(self, prediction_horizon_size, history_dimension):
         self.prediction_horizon_size = prediction_horizon_size
         self.model = None
         self.cnn_output_size = 256
@@ -25,12 +25,15 @@ class WorldModel(object):
         self.loss = categorical_crossentropy
         self.batch_size = 256
         self.num_epochs = 100
+        self.h_observations = list()
+        self.h_actions = list()
+        self.history_dimension = history_dimension
 
-        self.conv_net = self.build_conv_net()
-        self.gru_net = self.build_gru_net()
-        self.mlp_net = [self.build_mlp_net() for i in range(self.prediction_horizon_size)]
+        self.conv_net = self._build_conv_net()
+        self.gru_net = self._build_gru_net()
+        self.mlp_net = [self._build_mlp_net() for _ in range(self.prediction_horizon_size)]
 
-    def build_conv_net(self):
+    def _build_conv_net(self):
         cnn_model = Sequential()
         cnn_model.add(Conv2D(filters=self.cnn_l1_num_filters,
                              kernel_size=self.cnn_l1_kernel_size,
@@ -44,13 +47,13 @@ class WorldModel(object):
         cnn_model.compile(optimizer=self.optimizer, loss=self.loss, metrics=['accuracy'])
         return cnn_model
 
-    def build_gru_net(self):
+    def _build_gru_net(self):
         gru_model = Sequential()
         gru_model.add(GRU(units=self.gru_num_units))
         gru_model.compile(optimizer=self.optimizer, loss=self.loss, metrics=['accuracy'])
         return gru_model
 
-    def build_mlp_net(self):
+    def _build_mlp_net(self):
         mlp_model = Sequential()
         mlp_model.add(Dense(units=self.mlp_hidden_size, activation='relu'))
         mlp_model.add(Dense(units=self.mlp_output_size, activation='softmax'))
@@ -85,53 +88,22 @@ class WorldModel(object):
         """
         return self.mlp_net[k].predict(np.concatenate((b, a)))
 
-    def calc_loss(self, p, o):
+    def train(self):
         """
-        Calculate loss function
-        :param p: probability
-        :param o: observation
+        Train World Model network.
         :return:
         """
         pass
-
-    def train(self, h_t, K=10):
-        """
-        Train World Model network.
-        :param h_t: History: h_t = (o_0, a_0, o_1, a_1, ..., a_t-1, o_t)
-        :param K: number of frame predictions
-        :return:
-        """
-        T = len(h_t)
-        b = 0
-        o = [h_t[idx] for idx in range(0, len(h_t), 2)]
-        a = [h_t[idx] for idx in range(1, len(h_t), 2)]
-        for t in range(1, T - K):
-            A = [a[t - 1]]
-            P = list()
-            L = list()
-            z_t = self.f_fi(o[t])
-            b_t = self.f_theta(z_t=z_t, a_t_1=a[t - 1], b_t_1=b)
-            b = b_t
-            for k in range(1, K):
-                p = self.f_xi(k=k, b=b, a=A)
-                A.append(a[t + k - 1])
-                P.append(p)
-                L.append(self.calc_loss(p, o[t + k]))
-            self.update_reward()
-        self.update_weights()
-        self.update_policy()
 
     def summary(self):
         self.model.summary()
 
-
-class EvaluationNetwork(object):
-    def __init__(self):
-        pass
-
-
-class RewardNetwork(object):
-    def __init__(self):
+    def predict(self, actions_sequence):
+        """
+        Predict K frames, given sequence of actions
+        :param actions_sequence: a list of actions
+        :return: probability of future observations, given actions
+        """
         pass
 
 
@@ -172,14 +144,21 @@ class Obstacle(object):
 
 
 class AgentObservation(object):
-    def __init__(self, num_obstacles, obstacles_in_fov, walls_in_fov):
-        self.walls = list()
-        self.obstacles = list()
-        for ob in obstacles_in_fov:
-            self.obstacles.append(ob)
-        for wall in walls_in_fov:
-            self.walls.append(wall)
-        self.num_obstacles = num_obstacles
+    def __init__(self, car_pos, walls, cars):
+        self.car_pos = car_pos
+        self.walls = walls
+        self.cars = cars
+        self.obstacle_array = self._build_observation()
+
+    def _build_observation(self):
+        obstacle_arr = list()
+        obstacle_arr.append(self.walls)
+        for car in self.cars:
+            if car.is_obstacle_in_agent_fov(self.car_pos) is True:
+                obstacle_arr.append(car.encoding)
+            else:
+                obstacle_arr.append([0, 0, 0, 0, 0])
+        return obstacle_arr
 
 
 class AgentAction(object):
@@ -189,12 +168,3 @@ class AgentAction(object):
     LEFT = np.array([0, 0, 0, 1, 0])
     RIGHT = np.array([0, 0, 0, 0, 1])
 
-    def __init__(self, action=STAY):
-        self.action = action
-
-
-if __name__ == "__main__":
-    model = WorldModel(prediction_horizon_size=10)
-    conv = model.build_conv_net()
-    gru = model.build_gru_net()
-    mlp = model.build_mlp_net()
