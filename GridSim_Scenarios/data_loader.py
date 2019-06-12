2c19d477c0bc7be42a2f1a4250e5ff192db25bc2
Cosmin Ginerica <cosmin.ginerica@elektrobit.com>
2019-06-12 18:08:12 +0300

    Fixed dimensionality problems

--- GridSim_Scenarios/data_loader.py
+++ GridSim_Scenarios/data_loader.py
@@ -3,16 +3,22 @@ import numpy as np
 
 
 class StateEstimationDataGenerator(Sequence):
-    def __init__(self, actions_file, observations_file, predictions_file, batch_size):
+    def __init__(self, actions_file, observations_file, predictions_file, batch_size, history_size, prediction_horizon_size):
         self.batch_size = batch_size
         self.action_file = actions_file
         self.observation_file = observations_file
         self.prediction_file = predictions_file
         self.num_samples = self.__get_num_samples()
+        self.history_size = history_size
+        self.prediction_horizon_size = prediction_horizon_size
         self.last_fp_actions = 0
         self.last_fp_predictions = 0
         self.last_fp_observations = 0
-        self.prev_action = 0.0
+        self.print_generator_details = True
+        if self.print_generator_details:
+            print("State Estimation Generator: number of samples: {0}, batch_size: {1}, num_steps: {2}".format(
+                self.num_samples, self.batch_size, self.__len__()
+            ))
 
     def __get_num_samples(self):
         with open(self.action_file, "r") as f:
@@ -52,10 +58,27 @@ class StateEstimationDataGenerator(Sequence):
             pass
         return f_elems
 
+    def process_data(self):
+        history = list()
+        actions = list()
+        predictions = list()
+        prev_actions = list()
+        with open(self.observation_file, "r") as obs_f:
+            with open(self.prediction_file, "r") as pred_f:
+                with open(self.action_file, "r") as action_f:
+                    obs_f.seek(self.last_fp_observations)
+                    pred_f.seek(self.last_fp_predictions)
+                    action_f.seek(self.last_fp_actions)
+                    batch_idx = 0
+                    while batch_idx < self.batch_size:
+                        pass
+
     def __getitem__(self, item):
         observations = list()
         actions = list()
         predictions = list()
+        prev_actions = list()
+        prev_actions.append(0.0)
         with open(self.observation_file, "r") as obs_f:
             with open(self.prediction_file, "r") as pred_f:
                 with open(self.action_file, "r") as action_f:
@@ -64,58 +87,27 @@ class StateEstimationDataGenerator(Sequence):
                     action_f.seek(self.last_fp_actions)
                     idx = 0
                     while idx < self.batch_size:
-                        observations.append(self._process_obs(obs_f.readline()))
+                        obs = self._process_obs(obs_f.readline())
+                        observations.append(obs)
                         actions.append(self._process_action(action_f.readline()))
                         predictions.append(self._process_prediction(pred_f.readline()))
-                        self.prev_action = actions[0]
+                        idx += 1
+                    for idx in range(1, len(actions)):
+                        prev_actions.append(actions[idx][-1])
                     self.last_fp_predictions = pred_f.tell()
                     self.last_fp_actions = action_f.tell()
                     self.last_fp_observations = obs_f.tell()
-        return [observations, actions, self.prev_action], predictions
 
+        p = list()
+        for idx in range(len(predictions[0])):
+            pp = list()
+            for idx2 in range(len(predictions)):
+                pp.append(predictions[idx2][idx])
+            p.append(np.array(pp).reshape((len(predictions), 1)))
 
-class DataGenerator(Sequence):
-    def __init__(self, list_ids, labels, batch_size=32, dim=(32, 32, 32), n_channels=1, n_classes=10, shuffle=True):
-        self.dim = dim
-        self.batch_size = batch_size
-        self.labels = labels
-        self.list_IDs = list_ids
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.shuffle = shuffle
-        self.indexes = None
-        self.on_epoch_end()
-
-    def __len__(self):
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
-
-    def __getitem__(self, index):
-        #  Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-
-        # Find list of IDs
-        list_ids_temp = [self.list_IDs[k] for k in indexes]
-
-        # Generate data
-        x, y = self.__data_generation(list_ids_temp)
-
-        return x, y
+        return [np.array(observations), np.array(actions), np.array(prev_actions)], p
 
     def on_epoch_end(self):
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle:
-            np.random.shuffle(self.indexes)
-
-    def __data_generation(self, list_ids_temp):
-        # Generates data containing batch_size samples: (n_samples, *dim, n_channels)
-        x = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty(self.batch_size, dtype=float)
-
-        # Generate data
-        for i, ID in enumerate(list_ids_temp):
-            # Store sample
-            x[i, ] = np.load('data/' + ID + '.npy')
-
-            # Store class
-            y[i] = self.labels[ID]
-        return x, y
+        self.last_fp_observations = 0
+        self.last_fp_actions = 0
+        self.last_fp_predictions = 0
--- GridSim_Scenarios/simplified_world_model_network.py
+++ GridSim_Scenarios/simplified_world_model_network.py
@@ -73,40 +73,41 @@ def split_data_train_labels(h_t, a_t, prediction_horizon_size):
 
 
 class WorldModel(object):
-    def __init__(self, input_shape, prediction_horizon_size):
-        self.input_shape = input_shape
+    def __init__(self, prediction_horizon_size, history_size):
+        self.input_shape = (history_size, 2)  # 2 - observation size (pos_y_dif, in_fov)
         self.input_layer_num_units = 8
         self.mlp_layer_num_units = 8
-        self.gru_layer_num_units = 128
+        self.gru_layer_num_units = 32
         self.mlp_hidden_layer_size = prediction_horizon_size
+        self.history_size = history_size
         self.mlp_output_layer_size = 1
         self.model = None
         self.action_shape = (prediction_horizon_size,)
         self.batch_size = 32
+        self.print_summary = True
         self._build_architecture()
 
     def _build_architecture(self):
-        input_shape = Input(shape=self.input_shape)
+        input_layer = Input(shape=self.input_shape)
         action_layer = Input(shape=self.action_shape)
-        prev_action = Input(shape=(1,))
-        gru_input = Concatenate()([input_shape, prev_action])
+        prev_action = Input(shape=(self.history_size, 1))
+        gru_input = Concatenate()([input_layer, prev_action])
         input_gru = Reshape(target_shape=(1, int(gru_input.shape[1])))(gru_input)
         gru = GRU(units=self.gru_layer_num_units)(input_gru)
         mlp_outputs = list()
         for idx in range(self.mlp_hidden_layer_size):
-            #mlp_inputs = action_layer[:idx + 1]
-            mlp_inputs = Lambda(lambda x, i: x[:i])
-            mlp_inputs.arguments = {'i': idx}
-            mlp_inputs = mlp_inputs(action_layer)
+            mlp_inputs = Lambda(lambda x: x[:, :idx+1])(action_layer)
             mlp_in = Concatenate()([gru, mlp_inputs])
             mlp = Dense(units=self.mlp_layer_num_units, activation="relu")(mlp_in)
             mlp_output = Dense(units=self.mlp_output_layer_size, activation="relu")(mlp)
             mlp_outputs.append(mlp_output)
 
-        self.model = Model([input_shape, action_layer, prev_action], mlp_outputs)
-        self.model.compile(optimizer=Adam(lr=0.00005), loss="categorical_crossentropy", metrics=["accuracy"])
+        self.model = Model([input_layer, action_layer, prev_action], mlp_outputs)
+        self.model.compile(optimizer=Adam(lr=0.00005), loss="mean_squared_error", metrics=["accuracy"])
 
     def train_network(self, epochs=10, batch_size=256):
+        if self.print_summary:
+            self.model.summary()
         generator = StateEstimationDataGenerator(actions_file=os.path.join(os.path.dirname(__file__),
                                                                            "resources",
                                                                            "traffic_cars_data",
@@ -139,6 +140,22 @@ class WorldModel(object):
 
 
 if __name__ == "__main__":
-    model = WorldModel(input_shape=(2,), prediction_horizon_size=10)
-    model.train_network(epochs=10, batch_size=32)
+    '''bp = os.path.join(os.path.dirname(__file__), "resources", "traffic_cars_data")
+    fn = ["index", "ego_x", "ego_y", "ego_angle", "ego_acceleration", "ego_vel", "num_tracked_cars"]
+    idx = 0
+    fn.append("car_{0}_x".format(idx))
+    fn.append("car_{0}_y".format(idx))
+    fn.append("car_{0}_angle".format(idx))
+    fn.append("car_{0}_acceleration".format(idx))
+    fn.append("car_{0}_vel".format(idx))
+    fn.append("car_{0}_in_fov".format(idx))
+    fn.append("car_{0}_d_y".format(idx))
+    obs_p = os.path.join(bp, "state_estimation_data", "observations.npy")
+    pred_p = os.path.join(bp, "state_estimation_data", "predictions.npy")
+    act_p = os.path.join(bp, "state_estimation_data", "actions.npy")
 
+    merge_training_files(base_path=bp, fieldnames=fn, prediction_horizon_size=10, history_size=10,
+                         output_path_observations=obs_p, output_path_actions=act_p, output_path_predictions=pred_p)'''
+
+    model = WorldModel(prediction_horizon_size=10, history_size=10)
+    model.train_network(epochs=30, batch_size=32)
