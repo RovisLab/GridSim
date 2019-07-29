Added scripts for model car data processing; Added normalization script; Fixed evaluate_model script

GridSim_Scenarios/model_car_data_loader.py
@@ -1,18 +1,92 @@
-from keras.utils import Sequence
-from keras.preprocessing.sequence import pad_sequences
 import os
-import numpy as np
-import random
 import cv2
+import numpy as np
+from keras.utils import Sequence
 
 
-class StateEstimationDataGeneratorImpl(object):
+def get_model_car_observations(base_path, obs_str):
+    raw_elements = obs_str.split(",")
+    processed_elements = list()
+    if len(raw_elements[-1]) == 0:
+        raw_elements.pop()
+    for idx in range(0, len(raw_elements), 7):
+        us_fl = float(raw_elements[idx])
+        us_fcl = float(raw_elements[idx+1])
+        us_fc = float(raw_elements[idx+2])
+        us_fcr = float(raw_elements[idx+3])
+        us_fr = float(raw_elements[idx+4])
+        img_rgb = cv2.imread(os.path.join(base_path, raw_elements[idx+5]))
+        img_d = cv2.imread(os.path.join(base_path, raw_elements[idx+6]))
+        processed_elements.append((us_fl, us_fcl, us_fc, us_fcr, us_fr, img_rgb, img_d))
+    return processed_elements
+
+
+def get_model_car_predictions(pred_str):
+    raw_elements = pred_str.split(",")
+    processed_elements = list()
+    if len(raw_elements[-1]) == 0:
+        raw_elements.pop()
+    for idx in range(0, len(raw_elements), 5):
+        us_fl = float(raw_elements[idx])
+        us_fcl = float(raw_elements[idx + 1])
+        us_fc = float(raw_elements[idx + 2])
+        us_fcr = float(raw_elements[idx + 3])
+        us_fr = float(raw_elements[idx + 4])
+        processed_elements.append((us_fl, us_fcl, us_fc, us_fcr, us_fr))
+    return processed_elements
+
+
+def get_model_car_actions(action_str):
+    raw_elements = action_str.split(",")
+    processed_elements = list()
+    if len(raw_elements[-1]) == 0:
+        raw_elements.pop()
+    for idx in range(0, len(raw_elements), 2):
+        vel = float(raw_elements[idx])
+        steering_angle = float(raw_elements[idx+1])
+        processed_elements.append((vel, steering_angle))
+    return processed_elements
+
+
+def get_model_car_prev_actions(prev_action_str):
+    processed_elements = list()
+    raw_elements = prev_action_str.split(",")
+    if len(raw_elements[-1]) == 0:
+        raw_elements.pop()
+    for x in raw_elements:
+        mini_list = list()
+        for xx in x:
+            mini_list.append(float(xx))
+        processed_elements.append(mini_list)
+    return processed_elements
+
+
+class StateEstimationModelCarDataGenerator(Sequence):
+    """
+    Observation File
+    (us_fl, us_fcl, us_fc, us_fcr, us_fr, img_rgb, img_d), ...
+    ...
+
+    Action File
+    (velocity, steering_angle, ), ...
+    ...
+
+    Previous Action File
+    (velocity, steering_angle)
+    ...
+
+    Prediction File
+    (us_fl, us_fcl, us_fc, us_fcr, us_fr), ...
+    ...
+    """
     def __init__(self, input_file_path, batch_size, prediction_horizon_size, shuffle=True, validation=False):
+        self.input_file_path = input_file_path
+        self.rovis_desc_file = os.path.join(self.input_file_path, "desc_file.csv")
+        self.sensor_desc_file = os.path.join(self.input_file_path, "sensor_desc.csv")
         self.batch_size = batch_size
+        self.prediction_horizon_size = prediction_horizon_size
         self.shuffle = shuffle
         self.validation = validation
-        if self.validation:
-            self.shuffle = False
         self.action_file = os.path.join(input_file_path, "actions.npy") \
             if not self.validation else os.path.join(input_file_path, "actions_val.npy")
 
@@ -30,10 +104,13 @@ class StateEstimationDataGeneratorImpl(object):
             else self.num_samples - (self.num_samples % batch_size)
         self.prediction_horizon_size = prediction_horizon_size
         self.print_generator_details = True
-        self.file_markers = list()  # (obs, act, prev_act, pred)
+        self.file_markers = list()
         self.file_markers.append((0, 0, 0, 0))
         self.cache_file_markers = list()
         self.__get_file_markers()
+
+    def __len__(self):
+        return int(np.floor(self.num_samples / self.batch_size))
 
     def __get_num_samples(self):
         with open(self.action_file, "r") as f:
@@ -63,68 +140,26 @@ class StateEstimationDataGeneratorImpl(object):
             self.cache_file_markers.append(self.file_markers[i])
 
     def read_observations(self, obs_str):
-        elements = list()
-        for elem in obs_str.split(","):
-            try:
-                elements.append(float(elem))
-            except ValueError:
-                pass
-        observations = list()
-        in_fovs = list()
-        for h_idx in range(0, len(elements), 2):
-            observations.append(elements[h_idx])
-            in_fovs.append(elements[h_idx + 1])
-        return observations, in_fovs
-
-    def read_actions(self, action_str):
-        elements = list()
-        for elem in action_str.split(","):
-            try:
-                elements.append(float(elem))
-            except ValueError:
-                pass
-        actions = list()
-        if len(elements) == self.prediction_horizon_size:
-            for act_idx in range(self.prediction_horizon_size):
-                actions.append(elements[act_idx])
-        return actions
+        return get_model_car_observations(base_path=self.input_file_path, obs_str=obs_str)
 
     def read_predictions(self, pred_str):
-        elements = list()
-        for elem in pred_str.split(","):
-            try:
-                elements.append(float(elem))
-            except ValueError:
-                pass
-        predictions = list()
-        if len(elements) == self.prediction_horizon_size:
-            for pred_idx in range(self.prediction_horizon_size):
-                predictions.append(elements[pred_idx])
-        return predictions
+        return get_model_car_predictions(pred_str=pred_str)
 
-    def read_prev_actions(self, prev_action_str):
-        elements = list()
-        for elem in prev_action_str.split(","):
-            try:
-                elements.append(float(elem))
-            except ValueError:
-                pass
-        return elements
+    def read_actions(self, action_str):
+        return get_model_car_actions(action_str=action_str)
 
-    def __len__(self):
-        return int(np.floor(self.num_samples / self.batch_size))
+    def read_prev_actions(self, prev_act_str):
+        return get_model_car_prev_actions(prev_action_str=prev_act_str)
 
     def get_batch(self):
         actions = list()
         observations = list()
-        in_fovs = list()
         prev_actions = list()
         predictions = list()
         with open(self.action_file, "r") as act_f:
             with open(self.prediction_file, "r") as pred_f:
                 with open(self.prev_action_file, "r") as prev_act_f:
                     with open(self.observation_file, "r") as obs_f:
-                        # (obs, act, prev_act, pred)
                         obs_f.seek(self.file_markers[0][0])
                         act_f.seek(self.file_markers[0][1])
                         prev_act_f.seek(self.file_markers[0][2])
@@ -135,51 +170,30 @@ class StateEstimationDataGeneratorImpl(object):
                             crt_actions = self.read_actions(act_f.readline())
                             crt_prev_actions = self.read_prev_actions(prev_act_f.readline())
                             crt_predictions = self.read_predictions(pred_f.readline())
-                            crt_observations, crt_in_fovs = self.read_observations(obs_f.readline())
+                            crt_observations = self.read_observations(obs_f.readline())
                             if len(crt_actions) > 0:
                                 actions.append(crt_actions)
                             if len(crt_observations) > 0:
                                 observations.append(crt_observations)
-                            if len(crt_in_fovs) > 0:
-                                in_fovs.append(crt_in_fovs)
-                            if len(crt_prev_actions) > 0:
-                                prev_actions.append(crt_prev_actions)
+                            # if len(crt_prev_actions) > 0:
+                            prev_actions.append(crt_prev_actions)
                             if len(crt_predictions) > 0:
                                 predictions.append(crt_predictions)
                             idx += 1
                         self.file_markers.pop(0)
-        return actions, observations, in_fovs, prev_actions, predictions
-
-
-class StateEstimationDataGenerator(Sequence):
-    def __init__(self, input_file_path, batch_size, prediction_horizon_size, shuffle=True, validation=False):
-        self.__impl__ = StateEstimationDataGeneratorImpl(input_file_path=input_file_path,
-                                                         batch_size=batch_size,
-                                                         prediction_horizon_size=prediction_horizon_size,
-                                                         shuffle=shuffle,
-                                                         validation=validation)
-
-    def __len__(self):
-        return self.__impl__.__len__()
+        return actions, observations, prev_actions, predictions
 
     def __getitem__(self, item):
-        actions, observations, in_fovs, prev_actions, predictions = self.__impl__.get_batch()
-        return self._format_data(observations, in_fovs, actions, prev_actions, predictions)
+        actions, observations, prev_actions, predictions = self.get_batch()
+        return self._format_data(actions, observations, prev_actions, predictions)
 
-    def _format_data(self, observations, in_fovs, actions, prev_actions, predictions):
-        for idx in range(len(observations)):
-            for idx2 in range(len(observations[idx])):
-                observations[idx][idx2] = [observations[idx][idx2]]
-        for idx in range(len(in_fovs)):
-            for idx2 in range(len(in_fovs[idx])):
-                in_fovs[idx][idx2] = [in_fovs[idx][idx2]]
+    def _format_data(self, observations, actions, prev_actions, predictions):
         for idx in range(len(prev_actions)):
             for idx2 in range(len(prev_actions[idx])):
                 prev_actions[idx][idx2] = [prev_actions[idx][idx2]]
-        observations = pad_sequences(observations)
-        in_fovs = pad_sequences(in_fovs)
+        observations = np.array(observations)
         actions = np.array(actions)
-        prev_actions = pad_sequences(prev_actions)
+        prev_actions = np.array(prev_actions)
 
         p = list()
         if len(predictions):
@@ -188,17 +202,7 @@ class StateEstimationDataGenerator(Sequence):
                 for idx2 in range(len(predictions)):
                     pp.append(predictions[idx2][idx])
                 p.append(pp)
-
-        return [observations, in_fovs, actions, prev_actions], p
-
-    def reset_file_markers(self):
-        for i in range(len(self.__impl__.cache_file_markers)):
-            self.__impl__.file_markers.append(self.__impl__.cache_file_markers[i])
+        return [observations, actions, prev_actions], p
 
     def on_epoch_end(self):
-        self.__impl__.file_markers = list()
-        for i in range(len(self.__impl__.cache_file_markers)):
-            self.__impl__.file_markers.append(self.__impl__.cache_file_markers[i])
-        if self.__impl__.shuffle is True:
-            random.shuffle(self.__impl__.file_markers)
-
+        pass